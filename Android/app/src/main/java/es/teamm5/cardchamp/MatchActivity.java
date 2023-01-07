1bab1474032222a7881b9acb54cfc17f911f4e40
Ismaelds7234 <ismaeldominguezsanchez_7234@escuelaces.net>
2023-01-07 12:50:00 +0100

    Añadiendo Activity

---
+++ Android/app/src/main/java/es/teamm5/cardchamp/MatchActivity.java
@@ -0,0 +1,14 @@
+package es.teamm5.cardchamp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MatchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_match);
+    }
+}
